85bec770615a778d90e86d6e34145b76db610233
Dr Nic Williams <drnicwilliams@gmail.com>
2019-12-31 15:07:22 +1000

    remove unused debug

--- pkg/mutate/webhook.go
+++ pkg/mutate/webhook.go
@@ -34,10 +34,9 @@ func NodeSelectorMultiArch(clientset *kubernetes.Clientset, ignoredNamespaces []
 
 		// We handle all built-in Kinds that include a PodTemplateSpec, as described here:
 		// https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.15/#pod-v1-core
-		var namespace string
-		annotations := make(map[string]string)
 		var podSpec *core.PodSpec
-		// patch path is common for everything except raw Pod
+		// patch path is common for built-in resource definitions
+		// for everything except Pod
 		relativePatchPath := "/spec/template"
 
 		// Extract the necessary metadata from our known Kinds
@@ -47,8 +46,6 @@ func NodeSelectorMultiArch(clientset *kubernetes.Clientset, ignoredNamespaces []
 			if _, _, err := deserializer.Decode(admissionReview.Request.Object.Raw, nil, pod); err != nil {
 				return nil, err
 			}
-
-			namespace = pod.GetNamespace()
 			podSpec = &pod.Spec
 			relativePatchPath = ""
 		case "Deployment":
@@ -56,43 +53,31 @@ func NodeSelectorMultiArch(clientset *kubernetes.Clientset, ignoredNamespaces []
 			if _, _, err := deserializer.Decode(admissionReview.Request.Object.Raw, nil, &deployment); err != nil {
 				return nil, err
 			}
-
-			deployment.GetNamespace()
-			annotations = deployment.Spec.Template.GetAnnotations()
 			podSpec = &deployment.Spec.Template.Spec
 		case "StatefulSet":
 			statefulset := apps.StatefulSet{}
 			if _, _, err := deserializer.Decode(admissionReview.Request.Object.Raw, nil, &statefulset); err != nil {
 				return nil, err
 			}
-
-			namespace = statefulset.GetNamespace()
-			annotations = statefulset.Spec.Template.GetAnnotations()
 			podSpec = &statefulset.Spec.Template.Spec
 		case "DaemonSet":
 			daemonset := apps.DaemonSet{}
 			if _, _, err := deserializer.Decode(admissionReview.Request.Object.Raw, nil, &daemonset); err != nil {
 				return nil, err
 			}
-
-			namespace = daemonset.GetNamespace()
-			annotations = daemonset.Spec.Template.GetAnnotations()
 			podSpec = &daemonset.Spec.Template.Spec
 		case "Job":
 			job := batch.Job{}
 			if _, _, err := deserializer.Decode(admissionReview.Request.Object.Raw, nil, &job); err != nil {
 				return nil, err
 			}
-
-			namespace = job.Spec.Template.GetNamespace()
-			annotations = job.Spec.Template.GetAnnotations()
 			podSpec = &job.Spec.Template.Spec
 		default:
 			// TODO(drnic): except for whitelisted namespaces
 			return nil, xerrors.Errorf("the submitted Kind is not supported by this admission handler: %s", kind)
 		}
 
-		fmt.Printf("kind: %s, namespace: %s, annotations: %v\n", kind, namespace, annotations)
+		fmt.Printf("kind: %s\n", kind)
 
 		podInspect := NewFromPodSpec(clientset, podSpec, relativePatchPath)
 		podInspect.ApplyPatchToAdmissionResponse(resp)
